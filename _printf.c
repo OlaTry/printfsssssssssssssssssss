698c2580633ccf0bfe204dc7cb26ccf5b994c794
OlaTry <lila123456789ali@gmail.com>
2023-07-26 05:05:54 -0700

    try

--- _printf.c
+++ _printf.c
@@ -3,11 +3,11 @@
 void stoop(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * ptf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int ptf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
