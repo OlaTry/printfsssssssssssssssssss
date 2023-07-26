ca23b5aea51f1243cddfb199a8eb1557510f5cad
OlaTry <lila123456789ali@gmail.com>
2023-07-26 05:44:20 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void stoop(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int i, res = 0, prch = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			if (buff_ind == BUFF_SIZE)
 				stoop(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			prch++;
 		}
 		else
 		{
@@ -37,11 +37,11 @@ int _printf(const char *format, ...)
 			precision = get_tuning(format, &i, list);
 			size = get_length(format, &i);
 			++i;
-			printed = get_carbon(format, &i, list, buffer,
+			res = get_carbon(format, &i, list, buffer,
 				flags, width, precision, size);
-			if (printed == -1)
+			if (res == -1)
 				return (-1);
-			printed_chars += printed;
+			prch += res;
 		}
 	}
 
@@ -49,7 +49,7 @@ int _printf(const char *format, ...)
 
 	va_end(list);
 
-	return (printed_chars);
+	return (prch);
 }
 
 /**
