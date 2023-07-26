b83961f03e32c1c6c04fde0dd63a0a78f882cf65
OlaTry <lila123456789ali@gmail.com>
2023-07-26 06:09:17 -0700

    all

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * pc - Prints a char
+ * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -12,7 +12,7 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int pc(va_list types, char buffer[],
+int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
